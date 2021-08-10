3f26474f88ddc742541b996cfa4f8b328e8d8377
Yoni <yonatansamfisher@gmail.com>
2021-08-10 15:55:40 +0300

    added student Validation

---
+++ server/validation/studentValidation.js
@@ -0,0 +1,34 @@
+const Validator = require('validator');
+const isEmpty = require('is-empty');
+
+module.exports = function validateStudentData(data){
+    let errors = {};
+    //convert empty field to an empty string so we can use validator function
+    data.firstName = isEmpty(data.firstName) ? " ":data.firstName ;
+    data.lastName = isEmpty(data.lastName) ? " ":data.lastName ;
+    data.age = isEmpty(data.age) ? " ":data.age ;
+    data.email = isEmpty(data.email) ? " ":data.email ;
+    data.date = isEmpty(data.date) ? " ":data.date ;
+
+    //firstName checks
+    if(Validator.isEmpty(data.firstName)){
+        errors.firstName = "first Name field is required";
+    }
+    if(Validator.isEmpty(data.lastName)){
+        errors.firstName = "last Name field is required";
+    }
+    if(Validator.isEmpty(data.age)){
+        errors.firstName = "age field is required";
+    }
+    if(Validator.isEmail(data.email)){
+        errors.firstName = "email field is required";
+    }
+    if(Validator.isEmpty(data.date)){
+        errors.firstName = "date field is required";
+    }
+  
+    return{
+        errors,
+        isVaild:isEmpty(errors)
+    }
+}
